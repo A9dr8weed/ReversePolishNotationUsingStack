2fc3e5273aa756ac2e8f11d7ba25d39db3884510
Andrew <andsof123455@gmail.com>
2021-03-11 16:20:12 +0200

    Добавьте файлы проекта.

---
+++ ReversePolishNotationUsingStack/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+
+namespace ReversePolishNotationUsingStack
+{
+    public static class Program
+    {
+        private static void RpnLoop()
+        {
+            while (true)
+            {
+                Console.Write("> ");
+
+                string input = Console.ReadLine();
+                // If input == "quit", exit from loop.
+                if (string.Equals(input.Trim(), "quit", StringComparison.OrdinalIgnoreCase))
+                {
+                    break;
+                }
+
+                // The stack of values that have not yet been processed.
+                Stack values = new Stack();
+
+                // Enter input data via space.
+                foreach (string token in input.Split(new char[] { ' ' }))
+                {
+                    // If the value is an integer.
+                    if (int.TryParse(token, out int value))
+                    {
+                        // Put it on the stack.
+                        values.Push(value);
+                    }
+                    else
+                    {
+                        // Otherwise, perform the operation.
+                        int rhs = (int)values.Pop();
+                        int lhs = (int)values.Pop();
+
+                        // And put the result back.
+                        switch (token)
+                        {
+                            case "+":
+                                values.Push(lhs + rhs);
+                                break;
+                            case "-":
+                                values.Push(lhs - rhs);
+                                break;
+                            case "*":
+                                values.Push(lhs * rhs);
+                                break;
+                            case "/":
+                                values.Push(lhs / rhs);
+                                break;
+                            case "%":
+                                values.Push(lhs % rhs);
+                                break;
+                            default:
+                                // If the operation is not +, -, * or /
+                                throw new ArgumentException($"Unrecognized token: {token}");
+                        }
+                    }
+                }
+
+                // The last item on the stack is the result.
+                Console.WriteLine(values.Peek());
+            }
+        }
+
+        private static void Main()
+        {
+            RpnLoop();
+        }
+    }
+}
