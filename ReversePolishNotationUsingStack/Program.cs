3d1930c3d6fe19c0a22a8fe5e9834c918f44b0cd
Andrew <andsof123455@gmail.com>
2022-09-10 22:41:34 +0300

    Small refactoring

--- ReversePolishNotationUsingStack/Program.cs
+++ ReversePolishNotationUsingStack/Program.cs
@@ -14,9 +14,7 @@ namespace ReversePolishNotationUsingStack
                 string input = Console.ReadLine();
                 // If input == "quit", exit from loop.
                 if (string.Equals(input.Trim(), "quit", StringComparison.OrdinalIgnoreCase))
-                {
                     break;
-                }
 
                 // The stack of values that have not yet been processed.
                 Stack values = new Stack();
@@ -29,40 +27,42 @@ namespace ReversePolishNotationUsingStack
                     {
                         // Put it on the stack.
                         values.Push(value);
+                        continue;
                     }
-                    else
-                    {
-                        // Otherwise, perform the operation.
-                        int rhs = (int)values.Pop();
-                        int lhs = (int)values.Pop();
 
-                        // And put the result back.
-                        switch (token)
-                        {
-                            case "+":
-                                values.Push(lhs + rhs);
-                                break;
-                            case "-":
-                                values.Push(lhs - rhs);
-                                break;
-                            case "*":
-                                values.Push(lhs * rhs);
-                                break;
-                            case "/":
-                                values.Push(lhs / rhs);
-                                break;
-                            case "%":
-                                values.Push(lhs % rhs);
-                                break;
-                            default:
-                                // If the operation is not +, -, * or /
-                                throw new ArgumentException($"Unrecognized token: {token}");
-                        }
+                    if (values.Count <= 0)
+                        break;
+
+                    // Otherwise, perform the operation.
+                    int rhs = (int)values.Pop();
+                    int lhs = (int)values.Pop();
+
+                    // And put the result back.
+                    switch (token)
+                    {
+                        case "+":
+                            values.Push(lhs + rhs);
+                            break;
+                        case "-":
+                            values.Push(lhs - rhs);
+                            break;
+                        case "*":
+                            values.Push(lhs * rhs);
+                            break;
+                        case "/":
+                            values.Push(lhs / rhs);
+                            break;
+                        case "%":
+                            values.Push(lhs % rhs);
+                            break;
+                        default:
+                            // If the operation is not +, -, *, / or %
+                            throw new ArgumentException($"Unrecognized token: {token}");
                     }
                 }
 
                 // The last item on the stack is the result.
-                Console.WriteLine(values.Peek());
+                Console.WriteLine(values.Count > 0 ? values.Peek() : "Stack is empty.");
             }
         }
 
